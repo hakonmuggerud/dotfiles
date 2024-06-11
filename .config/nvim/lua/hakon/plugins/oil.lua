74c7d1a694953c34dfe3701b6b32d5464bd201bb
hakonmuggerud <hakon@videocation.no>
2024-06-11 21:12:48 +0200

    add precognition and update oil config

--- .config/nvim/lua/hakon/plugins/oil.lua
+++ .config/nvim/lua/hakon/plugins/oil.lua
@@ -9,7 +9,7 @@ return {
       }
     })
 
-    vim.keymap.set('n', '-', '<CMD>Oil<CR>')
-    vim.keymap.set('n', '<leader>-', require('oil').toggle_float)
+    vim.keymap.set('n', '<leader>-', '<CMD>Oil<CR>')
+    vim.keymap.set('n', '-', require('oil').toggle_float)
   end
 }
