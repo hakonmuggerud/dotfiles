0467969143abbe79ff5fb18f5e903bbb8fc2f362
hakonmuggerud <hakon@videocation.no>
2024-06-20 09:13:46 +0200

    Fix cmp setup

--- .config/nvim/lua/hakon/plugins/cmp.lua
+++ .config/nvim/lua/hakon/plugins/cmp.lua
@@ -1,37 +1,49 @@
 return {
-  -- Autocompletion
   'hrsh7th/nvim-cmp',
+  event = 'InsertEnter',
   dependencies = {
-    -- Adds LSP completion capabilities
+    {
+      'L3MON4D3/LuaSnip',
+      build = (function()
+        if vim.fn.has 'win32' == 1 or vim.fn.executable 'make' == 0 then
+          return
+        end
+        return 'make install_jsregexp'
+      end)(),
+    },
+    'saadparwaiz1/cmp_luasnip',
     'hrsh7th/cmp-nvim-lsp',
+    'hrsh7th/cmp-path',
   },
   config = function()
     local cmp = require('cmp')
+    local luasnip = require('luasnip')
+
+    luasnip.config.setup({})
 
     cmp.setup({
-      completion = {
-        completeopt = 'menu,menuone,noinsert'
+      snippet = {
+        expand = function(args)
+          luasnip.lsp_expand(args.body)
+        end,
       },
+      completion = { completeopt = 'menu,menuone,noinsert' },
+
       mapping = cmp.mapping.preset.insert {
         ['<C-n>'] = cmp.mapping.select_next_item(),
         ['<C-p>'] = cmp.mapping.select_prev_item(),
-        ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+
+        ['<C-b>'] = cmp.mapping.scroll_docs(-4),
         ['<C-f>'] = cmp.mapping.scroll_docs(4),
+
+        ['<C-y>'] = cmp.mapping.confirm { select = true },
+
         ['<C-Space>'] = cmp.mapping.complete {},
-        ['<CR>'] = cmp.mapping.confirm {
-          behavior = cmp.ConfirmBehavior.Replace,
-          select = true,
-        },
-        ['<S-Tab>'] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            cmp.select_prev_item()
-          else
-            fallback()
-          end
-        end, { 'i', 's' }),
       },
       sources = {
         { name = 'nvim_lsp' },
+        { name = 'luasnip' },
+        { name = 'path' },
       },
     })
   end
