1a484f4ed8ce5de8945489406e981c0ea2e3471a
hakonmuggerud <hakon@videocation.no>
2024-10-17 07:32:42 +0200

    add keymap for git hunk reset

--- .config/nvim/lua/hakon/plugins/gitsigns.lua
+++ .config/nvim/lua/hakon/plugins/gitsigns.lua
@@ -12,6 +12,7 @@ return {
     },
     on_attach = function(bufnr)
       vim.keymap.set('n', '<leader>hp', require('gitsigns').preview_hunk, { buffer = bufnr, desc = 'Preview git hunk' })
+      vim.keymap.set('n', '<leader>hr', require('gitsigns').reset_hunk, { buffer = bufnr, desc = 'Reset git hunk' })
 
       -- don't override the built-in and fugitive keymaps
       local gs = package.loaded.gitsigns
