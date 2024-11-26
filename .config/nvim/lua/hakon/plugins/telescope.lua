915d09e412b7d65a53d5992b36bd7c037ef4c4fc
Hakon Muggerud <hakon@videocation.no>
2024-11-26 18:34:00 +0100

    add telescope search

--- .config/nvim/lua/hakon/plugins/telescope.lua
+++ .config/nvim/lua/hakon/plugins/telescope.lua
@@ -73,6 +73,13 @@ return {
     vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
     vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
     vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
+    vim.keymap.set('n', '<leader>sa', function()
+      require('telescope.builtin').live_grep({
+        additional_args = function()
+          return { "--hidden" }
+        end,
+      })
+    end, { desc = '[S]earch [A]ll by grep' })
     vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
     vim.keymap.set('n', '<leader>sr', require('telescope.builtin').resume, { desc = '[S]earch [R]esume' })
     vim.keymap.set('n', '<leader>st', require('telescope.builtin').treesitter, { desc = '[S]earch [T]reesitter' })
