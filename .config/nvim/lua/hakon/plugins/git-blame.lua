607ac414a79258f00f0d7d06e65882b5537da400
hakonmuggerud <hakon@videocation.no>
2025-01-13 14:34:34 +0100

    add key-analyzer and grapple

--- .config/nvim/lua/hakon/plugins/git-blame.lua
+++ .config/nvim/lua/hakon/plugins/git-blame.lua
@@ -1,7 +1,7 @@
 vim.g.gitblame_date_format = '%r'
 vim.g.gitblame_display_virtual_text = 0
 
-vim.keymap.set('n', '<leader>go', '<cmd>GitBlameOpenCommitURL<cr>', { desc = 'Open commit url' })
+vim.keymap.set('n', '<leader>co', '<cmd>GitBlameOpenCommitURL<cr>', { desc = 'Open commit url' })
 
 return {
   'f-person/git-blame.nvim',
---
+++ .config/nvim/lua/hakon/plugins/grapple.lua
@@ -0,0 +1,19 @@
+return {
+  "cbochs/grapple.nvim",
+  dependencies = {
+    { "nvim-tree/nvim-web-devicons", lazy = true }
+  },
+  opts = {
+    scope = "git",
+  },
+  event = { "BufReadPost", "BufNewFile" },
+  cmd = "Grapple",
+  keys = {
+    { "<leader>g", "<cmd>Grapple toggle<cr>",          desc = "Grapple toggle tag" },
+    { "<leader>G", "<cmd>Grapple toggle_tags<cr>",     desc = "Grapple open tags window" },
+    { "<leader>n", "<cmd>Grapple cycle_tags next<cr>", desc = "Grapple cycle next tag" },
+    { "<leader>p", "<cmd>Grapple cycle_tags prev<cr>", desc = "Grapple cycle previous tag" },
+    { "<c-t>",     "<cmd>Grapple select index=1<cr>",  desc = "Grapple select tag 1" },
+    { "<c-y>",     "<cmd>Grapple select index=2<cr>",  desc = "Grapple select tag 2" },
+  },
+}
