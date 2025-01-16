5c0727c1dc3cd2feca40ff70c8471940358cf2d3
hakonmuggerud <hakon@videocation.no>
2025-01-16 11:02:17 +0100

    update grapple config and add terraform lsp

--- .config/nvim/lua/hakon/plugins/grapple.lua
+++ .config/nvim/lua/hakon/plugins/grapple.lua
@@ -15,5 +15,7 @@ return {
     { "<leader>p", "<cmd>Grapple cycle_tags prev<cr>", desc = "Grapple cycle previous tag" },
     { "<c-t>",     "<cmd>Grapple select index=1<cr>",  desc = "Grapple select tag 1" },
     { "<c-y>",     "<cmd>Grapple select index=2<cr>",  desc = "Grapple select tag 2" },
+    { "<c-f>",     "<cmd>Grapple select index=3<cr>",  desc = "Grapple select tag 3" },
+    { "<c-b>",     "<cmd>Grapple select index=4<cr>",  desc = "Grapple select tag 4" },
   },
 }
